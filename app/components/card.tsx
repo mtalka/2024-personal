b7a86acc96d4ff82596b54ccd52b3125da814f15
Markus T <markus.talka@gmail.com>
2024-10-24 11:31:21 +0300

    Cards collapsible with styling

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+
 type CardProps = {
   title: string;
   subtitle?: string;
@@ -5,17 +9,25 @@ type CardProps = {
 };
 
 export default function Card({ title, subtitle, children }: CardProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div
       id="card"
-      className="shadow flex flex-col gap-4 p-4 rounded-lg bg-white"
+      className={`shadow flex flex-col ${isOpen && "gap-4"} p-4 rounded-lg bg-white cursor-pointer hover:-translate-y-px hover:shadow transition-transform hover:bg-[#fafafa]`}
+      onClick={() => setIsOpen(!isOpen)}
     >
       <div>
         <h3 className="font-bold">{title}</h3>
-        {subtitle && 
-        <span className="text-sm">{subtitle}</span>}
+        {isOpen && (subtitle && <span className="text-sm">{subtitle}</span>)}
       </div>
-      <div className="text-zinc-600 text-sm">{children}</div>
+      <div
+        className={`text-zinc-600 text-sm overflow-hidden transition-all duration-300 ease-in-out ${
+          isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
+        {children}
+      </div>
     </div>
   );
 }
